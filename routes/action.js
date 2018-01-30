6121222eaf417d770d7bca67ad0670c84759dced
vlad <sargsyan.vlad@gmail.com>
2018-01-30 15:01:29 +0400

    simplifying action.js code by deleting code duplications

--- routes/action.js
+++ routes/action.js
@@ -1,5 +1,5 @@
-const passport = require('passport');
-require('../config/passport')(passport);
+// const passport = require('passport');
+// require('../config/passport')(passport);
 const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
@@ -7,12 +7,12 @@ const Device = require("../models/device");
 
 
 
-
+//modified code, im deleted code duplication
 router.post('/:deviceId/:action',(req, res) => {
     let user = res.locals.users;
     let action = req.params.action;
     let deviceId = req.params.deviceId;
-    if(action === 'restart'){
+    if(action){
         Device.findOne({_id: deviceId},
             function (err, device) {
                 if (device) {
@@ -21,40 +21,7 @@ router.post('/:deviceId/:action',(req, res) => {
                     }).exec(function(error, data){
                         if(error) {
                             res.send(error)
-                        } if (user.restart == deviceId ) res.send(200,{msg:"Success"});
-                        else res.send(403,{msg:'You are not assigned to this action'});
-                    })
-                } else {
-                    return res.send(401,{message: 'Device Not found'});
-                }
-            })
-    }else if(action === 'start'){
-        Device.findOne({_id: deviceId},
-            function (err, device) {
-                if (device) {
-                    User.find({_id: user.id}).populate({
-                        path: deviceId
-                    }).exec(function(error, data){
-                        if(error) {
-                            res.send(error)
-                        } if (user.start == deviceId ){res.send(user.stop) }
-                        else res.send(403,{msg:'You are not assigned to this action'});
-                    })
-                } else {
-                    return res.send(401,{message: 'Device Not found'});
-                }
-            })
-    }
-    else if(action === 'stop'){
-        Device.findOne({_id: deviceId},
-            function (err, device) {
-                if (device) {
-                    User.find({_id: user.id}).populate({
-                        path: deviceId
-                    }).exec(function(error, data){
-                        if(error) {
-                            res.send(error)
-                        } if (user.stop == deviceId ){res.send(user.stop) }
+                        } if (user[action] == deviceId ) res.send(200,{msg:"Success"});
                         else res.send(403,{msg:'You are not assigned to this action'});
                     })
                 } else {
