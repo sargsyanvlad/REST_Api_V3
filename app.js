03547d9570db61c88366775bb4ff0836c8932b2f
sargsyanvlad <sargsyan.vlad@gmail.com>
2018-04-13 11:02:55 +0400

    error handeler adder

--- app.js
+++ app.js
@@ -60,12 +60,15 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    if (err){
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    }
+    else next();
 });
 
 app.listen(8080, "127.0.0.1");
